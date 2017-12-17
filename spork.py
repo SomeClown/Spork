989b9a254e04a2cb589850551c2fac7a751d2717
SomeClown <justsomeclown@gmail.com>
2017-12-17 12:02:15 -0800

    Trying to organize better

--- spork.py
+++ spork.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 
-import os
-from ciscosparkapi import CiscoSparkAPI, SparkApiError
 from random import randint
 import time
 import click
 import datetime
-import json
-import pickle
-import csv
 from tqdm import tqdm
+from utilities import (
+    save_files,
+    get_my_rooms,
+    get_my_rooms_lst,
+    get_room_msg,
+    get_room_msg_lst,
+    name_to_id,
+    id_to_name,
+    init)
 
 __author__ = "SomeClown"
 __license__ = "MIT"
@@ -19,9 +23,6 @@ __email__ = "teren@wwt.com"
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 EPILOG = 'Have a fun time throwing foo\'s at bars'
-
-spark_token = os.environ["SPARK_ACCESS_TOKEN"]
-api = CiscoSparkAPI()
 
 color_black2 = "\033[1;30m"
 color_red2_on = "\033[01;31m"
@@ -41,108 +42,7 @@ def cli():
     Quick and dirty proof of concept. Exercises the Spark API without killing any kittens.
     :return: 
     """
-    pass
-
-
-def get_my_rooms():
-    """
-    Returns a list of rooms by ID
-    :return: 
-    """
-    rooms = [room for room in api.rooms.list()]
-    for room in rooms:
-        yield room
-
-
-def get_my_rooms_lst():
-    """
-    Returns a list of rooms by ID
-    :return: 
-    """
-    rooms = [room for room in api.rooms.list()]
-    return rooms
-
-
-def get_room_msg(room_id):
-    """
-    Returns messages from a given room ID
-    :param room_id: 
-    :return: 
-    """
-    room_messages = [item for item in api.messages.list(roomId=room_id) if item.files is not None]
-    for msg in room_messages:
-        yield msg
-
-
-def get_room_msg_lst(room_id):
-    """
-    Returns messages from a given room ID
-    :param room_id: 
-    :return: 
-    """
-    room_messages = [item for item in api.messages.list(roomId=room_id) if item.files is not None]
-    return room_messages
-
-
-def flatten(lst: list):
-    """
-    Not used at this point. Flattens a nested list. Also not quite working, so, there's that.
-    :param lst: 
-    :return: 
-    """
-    for element in lst:
-        if hasattr(element, "__iter__"):
-            yield from flatten(element)
-        elif element is not None:
-            yield element
-
-
-def save_files(data: dict, file_name: str, file_type: str):
-    """
-    Save data to file in a variety of formats
-    :param data: dictionary
-    :param file_name: file name
-    :param file_type: one of json, text, csv, pickle
-    :return: 
-    """
-    if file_type == 'json':
-        name = json.dumps(data)
-        with open(file_name, 'w') as f:
-            f.write(name)
-    elif file_type == 'pkl':
-        with open(file_name, 'wb') as f:
-            pickle.dump(data, f)
-    elif file_type == 'csv':
-        file = csv.writer(open(file_name, "w"))
-        for key, value in data.items():
-            file.writerow([key, value])
-    elif file_type == 'text':
-        with open(file_name, "w") as f:
-            f.write(str(data))
-
-
-def name_to_id(room_name: str):
-    """
-    Takes a room name and returns its id
-    :param room_name:
-    :return:
-    """
-    rooms_list = get_my_rooms_lst()
-    for room in rooms_list:
-        if room_name in room.title:
-            return room.id
-
-
-def id_to_name(room_id: str):
-    """
-    Takes a room id and returns its name
-    :param room_id:
-    :return:
-    """
-    rooms_list = get_my_rooms_lst()
-    for room in rooms_list:
-        if room_id in room.id:
-            return room.title
+    init()
 
 
 @click.command(options_metavar='[no options]', short_help='get list of files')
